989276e66590a1507308d0f332662be10b76122f
Memi Beltrame <memi@lessamess.ch>
2015-06-17 14:27:25 +0200

    iphone frames

--- snippets/footer.php
+++ snippets/footer.php
@@ -1,8 +1,8 @@
 <footer class="align-center">
         <br><br><br><hr>
-        <a href="<?php echo basename($_SERVER['PHP_SELF']) ;?>?session_renew=true"><i class="fa fa-refresh"></i></a>
+        <!-- <a href="<?php echo basename($_SERVER['PHP_SELF']) ;?>?session_renew=true"><i class="fa fa-refresh"></i></a>
         &nbsp;&nbsp;&nbsp;
-        <a href="index.php">H</a>
-        <a href="planer.php">Planer</a>
+        <a href="index.php">H</a> -->
+        <a href="planer.php">Planer</a>&nbsp;&nbsp;|&nbsp;&nbsp;
         <a href="index.php">Unternehmen</a>
 </footer>
